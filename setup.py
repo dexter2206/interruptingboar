034c803ee93d15b6ea9e49e3136b3bb8dc447ed0
Konrad Jałowiecki <dexter2206@gmail.com>
2019-08-20 13:30:45 +0200

    Add setup file

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 
 setup(
-    name='interruptingboar',
+    name="interruptingboar",
     use_scm_version=True,
-    author='Konrad Jałowiecki <dexter2206@gmail.com>',
-    setup_requires=['setuptools_scm']
+    author="Konrad Jałowiecki <dexter2206@gmail.com>",
+    setup_requires=["setuptools_scm"]
 )
