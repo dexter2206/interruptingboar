e71b6e99db227b054866216b406131139acc4877
Konrad Jałowiecki <dexter2206@gmail.com>
2019-08-20 13:51:01 +0200

    Add package finding

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name="interruptingboar",
     use_scm_version=True,
+    packages=find_packages(),
     author="Konrad Jałowiecki <dexter2206@gmail.com>",
     setup_requires=["setuptools_scm"]
 )
