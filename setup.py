095f3ef8c828947f155f15f4034f4e1ebdbcb978
Konrad Jałowiecki <dexter2206@gmail.com>
2019-08-20 14:17:51 +0200

    Cleanup setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ from setuptools import setup, find_packages
 setup(
     name="interruptingboar",
     use_scm_version=True,
+    url="https://github.com/dexter2206/interruptingboar",
+    platforms=["Linux", "Unix", "Windows"],
     packages=find_packages(),
-    author="Konrad Jałowiecki <dexter2206@gmail.com>",
+    author="Konrad Jałowiecki",
+    author_email="dexter2206@gmail.com",
     setup_requires=["setuptools_scm"]
 )
